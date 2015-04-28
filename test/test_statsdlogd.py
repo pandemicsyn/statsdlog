5f791fa261c7580063bc9f845978eb9d7ddcc522
pandemicsyn <syn@ronin.io>
2015-04-28 17:20:18 +0000

    pep8 test

--- test/test_statsdlogd.py
+++ test/test_statsdlogd.py
@@ -2,6 +2,7 @@ from mock import patch, Mock, MagicMock, sentinel, call
 from statsdlog.statsdlogd import StatsdLog
 import unittest
 
+
 class testit(unittest.TestCase):
 
     def setUp(self):
@@ -17,18 +18,20 @@ class testit(unittest.TestCase):
         file_mock.read.return_value = file_contents
         enter_mock = Mock()
         enter_mock.return_value = file_mock
-        exit_mock  = Mock()
-        setattr( context_manager_mock, '__enter__', enter_mock )
-        setattr( context_manager_mock, '__exit__', exit_mock )
+        exit_mock = Mock()
+        setattr(context_manager_mock, '__enter__', enter_mock)
+        setattr(context_manager_mock, '__exit__', exit_mock)
         result = StatsdLog(conf={'debug': 'y'})
         self.assertEquals(open_mock.call_args, call(file_path))
-        self.assertEquals(result.patterns, {'one': 'something.*', 'two': 'some.*'})
+        self.assertEquals(
+            result.patterns, {'one': 'something.*', 'two': 'some.*'})
         return result
 
     def test_check_line(self):
         self.assertEquals(self.sdl.check_line('no matches'), [])
         self.sdl.check_line('something matches')
-        self.assertEquals(sorted(self.sdl.check_line('something matches')), sorted(["one", "two"]))
+        self.assertEquals(
+            sorted(self.sdl.check_line('something matches')), sorted(["one", "two"]))
 
 if __name__ == '__main__':
     unittest.main()
